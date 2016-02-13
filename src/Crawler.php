e338b01aa2df49c695e34ee478ec6478a4370144
Midori Kocak <Midori.Kocak@massiveinteractive.com>
2016-02-13 21:27:29 +0100

    names

--- src/Crawler.php
+++ src/Crawler.php
@@ -39,9 +39,76 @@ use Goutte\Client;
 class Crawler {
 
     private $client;
+    private $crawler;
 
     function __construct()
     {
         $this->client = new Client();
+    }
+
+    function connect(){
+        $this->crawler = $this->client->request('GET', 'http://www.anitsayac.com/');
+    }
+
+    public function getNames(){
+            $names = $this->crawler->filter('span.xxy > a')->each(function ($node) {
+                return ['id'=>filter_var($node->attr('href'), FILTER_SANITIZE_NUMBER_INT),'name'=> $node->text(), 'link'=> 'http://www.anitsayac.com/'.$node->attr('href')];
+            });
+            $this->saveArrayToJson($names,'names');
+        return true;
+    }
+
+    public function getDetails(){
+        $namesJson = file_get_contents('data/names.json');
+        $result = [];
+        $names = json_decode($namesJson, true);
+        foreach($names as $key => $value){
+            var_dump($value['link']);
+            $person = $this->getPerson($value['link']);
+            $result[$key] = array_merge($person, $value);
+        }
+        $this->saveArrayToJson($result,'people');
+    }
+
+    function getPerson($url){
+        $result = [];
+        $personCrawler = $this->client->request('GET', $url);
+        $images = $personCrawler->filter('img')->each(function ($node) {
+            return $node->attr('src');
+        });
+        $body = $personCrawler->filter('body')->each(function ($node) {
+            return explode('<br>',$node->html());
+        });
+        $lastElementKey = null;
+        if(isset($body[0])){
+            foreach ($body[0] as $value){
+                $value = strip_tags($value);
+                if(strpos($value,':')!==false){
+                    $data = explode(':',$value);
+                    if(sizeof($data)>2){
+                        $data[1] .= $data[2];
+                    }
+                    $result[$data[0]] = trim($data[1]);
+                    $lastElementKey = $data[0];
+                }
+                else{
+                    $result[$lastElementKey] .= ' '.$value;
+                }
+            }
+        }
+        $result['images'] = $images;
+        return $result;
+    }
+
+    function saveArrayToJson(Array $array, String $name){
+        $encodedString = json_encode($array);
+        file_put_contents('data/'.$name.'.json', $encodedString);
+    }
+
+    function appendArrayToJson(Array $array, String $name){
+        $fileContents = file_get_contents('../data/'.$name.'.json');
+        $decoded = json_decode($fileContents, true);
+        $newArray = array_merge($decoded,$array);
+        file_put_contents('../data/'.$name.'.json', $newArray);
     }
 }
--- tests/CrawlerTest.php
+++ tests/CrawlerTest.php
@@ -4,4 +4,15 @@ use MidoriKocak\Crawler;
 
 class CrawlerTest extends \PHPUnit_Framework_TestCase {
 
+    public $crawler;
+
+    public function setup()
+    {
+        $this->crawler = new Crawler();
+        $this->crawler->connect();
+    }
+
+    public function testGetNames(){
+        $this->crawler->getDetails();
+    }
 } 
